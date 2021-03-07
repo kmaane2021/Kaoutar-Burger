fcfb5c69c4677421b76c3f61dcb27e689d319449
kaoutar <kmaane@hotmail.com>
2021-03-07 18:22:48 -0500

    Updates

---
+++ precache-manifest.c0ad7a282463da94bee36043c473fc3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "174ca0835887c3d791b1e817f4679fe5",
+    "url": "/kaoutar-burger/index.html"
+  },
+  {
+    "revision": "e4f9685a2c8f50d358b3",
+    "url": "/kaoutar-burger/static/css/main.bf049b1c.chunk.css"
+  },
+  {
+    "revision": "d27253d1df4d9c0fdbf9",
+    "url": "/kaoutar-burger/static/js/2.a996596a.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/kaoutar-burger/static/js/2.a996596a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4f9685a2c8f50d358b3",
+    "url": "/kaoutar-burger/static/js/main.33d3e2b5.chunk.js"
+  },
+  {
+    "revision": "d0ea21f81900db80516e",
+    "url": "/kaoutar-burger/static/js/runtime-main.20dcce99.js"
+  },
+  {
+    "revision": "edf5f47f61ade07daf683b7cde221827",
+    "url": "/kaoutar-burger/static/media/burger.edf5f47f.jpg"
+  }
+]);
